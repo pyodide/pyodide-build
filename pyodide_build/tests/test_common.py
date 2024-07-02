16574ac1deecb15c3435aa5c75955b62f6e644b0
Gyeongjae Choi <def6488@gmail.com>
2024-07-02 20:28:05 +0900

    Remove types-request, loky, and pyyaml from runtime dependencies (#6)
    This is a partial work for reducing the number of packages that
    pyodide-build depends on.
    
    - types-request: is only for development. Moved it to optional
    dependency.
    - loky: is used only for counting the CPU numbers. Vendored a few
    necessary parts to pyodide-build.
    - pyyaml: is replaced with ruamel.yaml
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- pyodide_build/tests/test_common.py
+++ pyodide_build/tests/test_common.py
@@ -7,7 +7,6 @@ from pyodide_build.common import (
     environment_substitute_args,
     extract_wheel_metadata_file,
     find_missing_executables,
-    get_num_cores,
     make_zip_archive,
     parse_top_level_import_name,
     repack_zip_archive,
@@ -69,16 +68,6 @@ def test_environment_var_substitution(monkeypatch):
         and args["cxxflags"] == "Robert Mc Roberts"
         and args["ldflags"] == '"-lpyodide_build_dir"'
     )
-
-
-@pytest.mark.parametrize("num_cpus", [1, 2, 3])
-def test_get_num_cores(monkeypatch, num_cpus):
-    import loky
-
-    with monkeypatch.context() as m:
-        m.setattr(loky, "cpu_count", lambda: num_cpus)
-
-        assert get_num_cores() == num_cpus
 
 
 @pytest.mark.parametrize(
