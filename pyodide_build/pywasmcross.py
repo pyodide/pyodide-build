a84379d17b01b491f75f56a500758ffe7d5ccb79
Hood Chatham <roberthoodchatham@gmail.com>
2024-10-11 13:26:03 +0200

    Insert -Zbuild-std when building rust packages
    Otherwise we are linking a copy of the standard library built with some
    particular version of emscripten selected by the rust compiler that does not
    match the version we are using. This can lead to an ABI mismatch.
    
    See rust-lang/rust#131467

--- pyodide_build/pywasmcross.py
+++ pyodide_build/pywasmcross.py
@@ -525,6 +525,9 @@ def handle_command_generate_args(  # noqa: C901
     elif cmd == "strip":
         line[0] = "emstrip"
         return line
+    elif cmd == "cargo":
+        line.insert(1, "-Zbuild-std")
+        return line
     else:
         return line
 
