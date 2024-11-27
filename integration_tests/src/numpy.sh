712768ba592e2de590a9972092d6abce440aed71
Gyeongjae Choi <def6488@gmail.com>
2024-11-27 11:22:37 +0000

    Fix test [integration]

--- integration_tests/src/numpy.sh
+++ integration_tests/src/numpy.sh
@@ -5,7 +5,8 @@ set -e
 URL="https://files.pythonhosted.org/packages/65/6e/09db70a523a96d25e115e71cc56a6f9031e7b8cd166c1ac8438307c14058/numpy-1.26.4.tar.gz"
 
 wget $URL
-tar -xvf numpy-1.26.4.tar.gz
+tar -xf numpy-1.26.4.tar.gz
 cd numpy-1.26.4
 
-pyodide build
+MESON_CROSS_FILE=`pyodide config get meson_cross_file`
+pyodide build -Csetup-args=-Dallow-noblas=true -Csetup-args=--cross-file=${MESON_CROSS_FILE}
