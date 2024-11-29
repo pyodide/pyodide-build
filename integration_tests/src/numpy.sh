769fcc23a537d7226f8f3d1891825b988090515f
Gyeongjae Choi <def6488@gmail.com>
2024-11-29 23:15:11 +0900

    lint

--- integration_tests/src/numpy.sh
+++ integration_tests/src/numpy.sh
@@ -8,5 +8,5 @@ wget $URL
 tar -xf numpy-1.26.4.tar.gz
 cd numpy-1.26.4
 
-MESON_CROSS_FILE=`pyodide config get meson_cross_file`
-pyodide build -Csetup-args=-Dallow-noblas=true -Csetup-args=--cross-file=${MESON_CROSS_FILE}
+MESON_CROSS_FILE=$(pyodide config get meson_cross_file)
+pyodide build -Csetup-args=-Dallow-noblas=true -Csetup-args=--cross-file="${MESON_CROSS_FILE}"
