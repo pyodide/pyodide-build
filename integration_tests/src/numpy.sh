569e88d8cb87e3f1d8d5c893efae8240ebf39822
Gyeongjae Choi <def6488@gmail.com>
2024-12-02 22:27:46 +0900

    Update integration_tests/src/numpy.sh
    Co-authored-by: Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>

--- integration_tests/src/numpy.sh
+++ integration_tests/src/numpy.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-URL="https://files.pythonhosted.org/packages/65/6e/09db70a523a96d25e115e71cc56a6f9031e7b8cd166c1ac8438307c14058/numpy-1.26.4.tar.gz"
+URL="https://files.pythonhosted.org/packages/source/n/numpy/numpy-1.26.4.tar.gz"
 
 wget $URL
 tar -xf numpy-1.26.4.tar.gz
