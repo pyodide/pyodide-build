7aab0432fa9318211baffac0f9b9e58514478368
Gyeongjae Choi <def6488@gmail.com>
2025-03-13 12:47:33 +0000

    Basic support for uv

---
+++ pyodide_build/uv_helper.py
@@ -0,0 +1,28 @@
+import functools
+import shutil
+import os
+
+
+@functools.cache
+def find_uv_bin() -> str | None:
+    """
+    Check if the uv executable is available.
+
+    If the uv executable is available, return the path to the executable.
+    Otherwise, return None.
+    """
+    try:
+        import uv
+
+        return uv.find_uv_bin()
+    except (ModuleNotFoundError, FileNotFoundError):
+        return shutil.which('uv')
+
+    return None
+
+
+def should_use_uv() -> bool:
+    # UV environ is set to the uv executable path when the script is called with the uv executable.
+    uv_environ = os.environ.get("UV")
+    # double check by comparing the uv executable path with the one found by the uv package.
+    return find_uv_bin() == uv_environ
