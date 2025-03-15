87c0c3b13c5d42f837bb82b928cc655c09898e59
Gyeongjae Choi <def6488@gmail.com>
2025-03-15 08:04:29 +0000

    check none

--- pyodide_build/uv_helper.py
+++ pyodide_build/uv_helper.py
@@ -25,4 +25,4 @@ def should_use_uv() -> bool:
     # UV environ is set to the uv executable path when the script is called with the uv executable.
     uv_environ = os.environ.get("UV")
     # double check by comparing the uv executable path with the one found by the uv package.
-    return find_uv_bin() == uv_environ
+    return uv_environ and uv_environ == find_uv_bin()
