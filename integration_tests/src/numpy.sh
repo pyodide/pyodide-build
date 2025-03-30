e38a704953522fcdc7366f612d52b4c4e77e798b
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2025-03-31 05:03:30 +0530

    Accept any args to `numpy.sh`

--- integration_tests/src/numpy.sh
+++ integration_tests/src/numpy.sh
@@ -2,17 +2,6 @@
 
 set -e
 
-NO_ISOLATION=""
-SKIP_DEPS=""
-
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        --no-isolation) NO_ISOLATION="--no-isolation"; shift ;;
-        --skip-dependency-check) SKIP_DEPS="--skip-dependency-check"; shift ;;
-        *) echo "Unknown parameter: $1"; exit 1 ;;
-    esac
-done
-
 VERSION="2.0.2"
 URL="https://files.pythonhosted.org/packages/source/n/numpy/numpy-${VERSION}.tar.gz"
 
@@ -21,4 +10,4 @@ tar -xf numpy-${VERSION}.tar.gz
 cd numpy-${VERSION}
 
 MESON_CROSS_FILE=$(pyodide config get meson_cross_file)
-${UV_RUN_PREFIX} pyodide build -Csetup-args=-Dallow-noblas=true -Csetup-args=--cross-file="${MESON_CROSS_FILE}" $NO_ISOLATION $SKIP_DEPS
+${UV_RUN_PREFIX} pyodide build -Csetup-args=-Dallow-noblas=true -Csetup-args=--cross-file="${MESON_CROSS_FILE}" "$@"
