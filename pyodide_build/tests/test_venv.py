pyodide/pyodide-build

commit 71a6d09651814439feb161537888078f7ff68c19

Make create_pip_script work with uv actually this time (#185)
Weird hack to work around:
https://github.com/astral-sh/python-build-standalone/issues/380
If we resolve the symlink all the way, the python-host interpreter works
but won't install into our pyodide venv. If we don't resolve the symlink,
sys.prefix is calculated incorrectly. To ensure that we get the right
sys.prefix, we explicitly set it with the PYTHONHOME environment variable
and then call the symlink.

pyodide_build/out_of_tree/venv.py
@@ -222,26 +222,45 @@ def create_pip_script(venv_bin):
     # Python in the shebang. Use whichever Python was used to invoke
     # pyodide venv.
     host_python_path = venv_bin / f"python{get_pyversion()}-host"
+    host_python_path_no_version = venv_bin / "python-host"
     pip_path = venv_bin / "pip_patched"
+    python_host_link = venv_bin / "python-host-link"
 
     # To support the "--clear" and "--no-clear" args, we need to remove
     # the existing symlinks before creating new ones.
     host_python_path.unlink(missing_ok=True)
-    (venv_bin / "python-host").unlink(missing_ok=True)
+    host_python_path_no_version.unlink(missing_ok=True)
+    python_host_link.unlink(missing_ok=True)
     for pip in venv_bin.glob("pip*"):
         if pip == pip_path:
             continue
         pip.unlink(missing_ok=True)
         pip.symlink_to(pip_path)
 
-    host_python_path.symlink_to(sys.executable)
-    # in case someone needs a Python-version-agnostic way to refer to python-host
-    (venv_bin / "python-host").symlink_to(sys.executable)
+    # Weird hack to work around:
+    # https://github.com/astral-sh/python-build-standalone/issues/380
+    # If we resolve the symlink all the way, the python-host interpreter works
+    # but won't install into our pyodide venv. If we don't resolve the symlink,
+    # sys.prefix is calculated incorrectly. To ensure that we get the right
+    # sys.prefix, we explicitly set it with the PYTHONHOME environment variable
+    # and then call the symlink.
+    python_host_link.symlink_to(sys.executable)
+    pythonhome = Path(sys._base_executable).parents[1]
+    host_python_path.write_text(
+        dedent(
+            f"""\
+            #!/bin/sh
+            exec env PYTHONHOME={pythonhome} {python_host_link} $@
+            """
+        )
+    )
+    host_python_path.chmod(0o777)
+    host_python_path_no_version.symlink_to(host_python_path)
 
     pip_path.write_text(
         # Other than the shebang and the monkey patch, this is exactly what
         # normal pip looks like.
-        f"#!{host_python_path} -s\n"
+        f"#!/usr/bin/env -S {host_python_path} -s\n"
         + get_pip_monkeypatch(venv_bin)
         + dedent(
             """

commit c4fc8d680f0067f76caae7aa748c2ab55a37cbe0

Fix virtual environment cli entrypoints
This fixes a regression introduced in #185.

pyodide_build/out_of_tree/venv.py
@@ -134,8 +134,11 @@ def get_pip_monkeypatch(venv_bin: Path) -> str:
         # to return the value with the host suffix removed.
         """
         from pip._vendor.distlib import scripts
+        EXECUTABLE_SUFFIX = "-host-link"
         def get_executable():
-            return sys.executable.removesuffix("-host")
+            if not sys.executable.endswith(EXECUTABLE_SUFFIX):
+                raise RuntimeError(f'Internal Pyodide error: expected sys.executable="{sys.executable}" to end with "{EXECUTABLE_SUFFIX}"')
+            return sys.executable.removesuffix(EXECUTABLE_SUFFIX)
 
         scripts.get_executable = get_executable
 

pyodide_build/tests/test_venv.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import subprocess
+from textwrap import dedent
 
 import pytest
 
@@ -256,3 +257,34 @@ def test_pip_downgrade(base_test_dir):
     assert result.returncode == 0, result.stderr
     assert result.stdout.startswith("pip 24.0")
     assert venv_pip_path.readlink() == venv_pip_path.with_name("pip_patched")
+
+
+def test_pytest_invoke(base_test_dir):
+    venv_path = base_test_dir / "test_venv"
+    venv.create_pyodide_venv(venv_path, [])
+    pip = venv_path / "bin" / "pip"
+    subprocess.run(
+        [
+            pip,
+            "install",
+            "pytest",
+        ],
+        check=True,
+    )
+    pytest = venv_path / "bin" / "pytest"
+
+    (base_test_dir / "test_a.py").write_text(
+        dedent(
+            """\
+            from pyodide.code import run_js
+
+            def test_run_js():
+                assert run_js("(x) => x + 1")(7) == 8
+            """
+        )
+    )
+    subprocess.run(
+        [pytest, base_test_dir / "test_a.py"],
+        check=True,
+        env=os.environ | {"_PYODIDE_EXTRA_MOUNTS": str(base_test_dir)},
+    )