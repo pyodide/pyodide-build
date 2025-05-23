356b3fa8ce97f602c4c9c785d77bc1eb3cbca173
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2025-05-23 12:54:20 +0530

    Extract URI conversion to a separate function
    Suggested-by: Gyeongjae Choi <def6488@gmail.com>

--- pyodide_build/build_env.py
+++ pyodide_build/build_env.py
@@ -14,7 +14,12 @@ from pathlib import Path
 from packaging.tags import Tag, compatible_tags, cpython_tags
 
 from pyodide_build import __version__
-from pyodide_build.common import default_xbuildenv_path, search_pyproject_toml, to_bool
+from pyodide_build.common import (
+    default_xbuildenv_path,
+    path_to_uri_if_spaces,
+    search_pyproject_toml,
+    to_bool,
+)
 
 RUST_BUILD_PRELUDE = """
 rustup default ${RUST_TOOLCHAIN}
@@ -334,15 +339,8 @@ def _create_constraints_file() -> str:
     if not constraints:
         return ""
 
-    # If a path to a file specified PIP_CONSTRAINT contains spaces, pip will misinterpret
-    # it as multiple files; see https://github.com/pypa/pip/issues/13283
-    # We work around this by converting the path to a URI.
     constraints_file = Path(constraints)
-    constraints = (
-        constraints_file.as_uri()
-        if " " in str(constraints_file)
-        else str(constraints_file)
-    )
+    constraints = path_to_uri_if_spaces(constraints_file)
 
     if not constraints_file.is_file():
         constraints_file.parent.mkdir(parents=True, exist_ok=True)
--- pyodide_build/recipe/builder.py
+++ pyodide_build/recipe/builder.py
@@ -40,6 +40,7 @@ from pyodide_build.common import (
     find_matching_wheel,
     make_zip_archive,
     modify_wheel,
+    path_to_uri_if_spaces,
     retag_wheel,
     retrying_rmtree,
 )
@@ -384,14 +385,7 @@ class RecipeBuilder:
             for constraint in constraints:
                 f.write(constraint + "\n")
 
-        # If a path to a file specified PIP_CONSTRAINT contains spaces, pip will misinterpret
-        # it as multiple files; see https://github.com/pypa/pip/issues/13283
-        # We work around this by converting the path to a URI.
-        new_constraints_str = (
-            new_constraints_file.as_uri()
-            if " " in str(new_constraints_file)
-            else str(new_constraints_file)
-        )
+        new_constraints_str = path_to_uri_if_spaces(new_constraints_file)
 
         if host_constraints:
             return host_constraints + " " + new_constraints_str
