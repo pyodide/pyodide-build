9c803e958709cdbdec615fcd2cdfae21006dca9a
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2025-06-30 21:08:00 +0530

    Add comment about SciPy 1.16 build problem
    Co-Authored-By: Gyeongjae Choi <def6488@gmail.com>

--- pyodide_build/_f2c_fixes.py
+++ pyodide_build/_f2c_fixes.py
@@ -77,8 +77,8 @@ def scipy_fix_cfile(path: Path) -> None:
     text = text.replace("static void f2py_setup_", "static int f2py_setup_")
 
     # Fix Cython-generated function pointer casts for __Pyx_IsSameCFunction
-    # for _matfuncs_sqrtm
-    # TODO: not sure if this is the best place to put this, but it works
+    # for _matfuncs_sqrtm for builds of SciPy 1.16.0.
+    # TODO: I'm not sure if this is the best place to put this, but it works
     text = re.sub(
         r"(__Pyx_IsSameCFunction\([^,]+,\s*)\(int\(\*\)\(void\)\)",
         r"\1(void(*)(void))",
