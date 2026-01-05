5e3b46e59de3e613b2bb0437750bd4c263975e34
Gyeongjae Choi <def6488@gmail.com>
2026-01-05 14:01:58 +0900

    Remove `create-zipfile` cli (#276)
    * remove pyzip cli
    
    * changelog

--- pyodide_build/tests/test_cli.py
+++ pyodide_build/tests/test_cli.py
@@ -13,7 +13,6 @@ from pyodide_build.cli import (
     build,
     build_recipes,
     config,
-    create_zipfile,
     py_compile,
     skeleton,
 )
@@ -266,65 +265,6 @@ def test_config_get(cfg_name, env_var, dummy_xbuildenv):
     )
 
     assert result.stdout.strip() == build_env.get_build_flag(env_var)
-
-
-def test_create_zipfile(temp_python_lib, temp_python_lib2, tmp_path):
-    from zipfile import ZipFile
-
-    output = tmp_path / "python.zip"
-
-    app = typer.Typer()
-    app.command()(create_zipfile.main)
-
-    result = runner.invoke(
-        app,
-        [
-            str(temp_python_lib),
-            str(temp_python_lib2),
-            "--output",
-            str(output),
-        ],
-    )
-
-    assert result.exit_code == 0, result.stdout
-    assert "Zip file created" in result.stdout
-    assert output.exists()
-
-    with ZipFile(output) as zf:
-        assert "module1.py" in zf.namelist()
-        assert "module2.py" in zf.namelist()
-        assert "module3.py" in zf.namelist()
-        assert "module4.py" in zf.namelist()
-
-
-def test_create_zipfile_compile(temp_python_lib, temp_python_lib2, tmp_path):
-    from zipfile import ZipFile
-
-    output = tmp_path / "python.zip"
-
-    app = typer.Typer()
-    app.command()(create_zipfile.main)
-
-    result = runner.invoke(
-        app,
-        [
-            str(temp_python_lib),
-            str(temp_python_lib2),
-            "--output",
-            str(output),
-            "--pycompile",
-        ],
-    )
-
-    assert result.exit_code == 0, result.stdout
-    assert "Zip file created" in result.stdout
-    assert output.exists()
-
-    with ZipFile(output) as zf:
-        assert "module1.pyc" in zf.namelist()
-        assert "module2.pyc" in zf.namelist()
-        assert "module3.pyc" in zf.namelist()
-        assert "module4.pyc" in zf.namelist()
 
 
 @pytest.mark.parametrize("target", ["dir", "file"])
