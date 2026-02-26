e6fb453d8c6bda9f75197c5d5ed1213fd025289d
Gyeongjae Choi <gyeongjae@cloudflare.com>
2026-02-26 15:30:11 +0900

    Vendor sharedlib by default

--- pyodide_build/recipe/spec.py
+++ pyodide_build/recipe/spec.py
@@ -89,7 +89,7 @@ class _BuildSpec(BaseModel):
     post: str | None = None
     unvendor_tests: bool = Field(True, alias="unvendor-tests")
     retain_test_patterns: list[str] = Field([], alias="_retain-test-patterns")
-    vendor_sharedlib: bool = Field(False, alias="vendor-sharedlib")
+    vendor_sharedlib: bool = Field(True, alias="vendor-sharedlib")
     cross_build_env: bool = Field(False, alias="cross-build-env")
     cross_build_files: list[str] = Field([], alias="cross-build-files")
     model_config = ConfigDict(extra="forbid")
